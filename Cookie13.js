7f752db067daf4d00e5bc4fa7629f60e28767b6f
5tudioDigital <online@studiodigital.at>
2021-05-04 15:01:40 +0200

    -

--- Cookie13.js
+++ Cookie13.js
@@ -88,9 +88,11 @@ alreadyLoaded ? document.getElementById("consentPopup").style.display = "none" :
 
 var latestPost = $('.section').find("a").attr('href');
 console.log(latestPost);
-var latestPostCookie = getCookie("LatestPost")
+setCookie("NotificationCenterShowed", false, 0.5);
+var latestPostCookie = getCookie("LatestPost");
+var getNotificationCenterStatus = getCookie("NotificationCenterShowed");
 
-if(latestPost === latestPostCookie) {
+if(latestPost === latestPostCookie & getNotificationCenterStatus === false) {
     var sameLatest = getCookie("sameLatest");
     switch (sameLatest) {
         case "1":
@@ -109,14 +111,17 @@ if(latestPost === latestPostCookie) {
             document.getElementById("NotificationCenter").click(); 
             console.log("NotificationCenter");
             setCookie("sameLatest", 1, 30);
+            setCookie("NotificationCenterShowed", true, 0.5);
           break;
         default:
             console.log("ERROR");
             setCookie("sameLatest", 1, 30);
+            setCookie("NotificationCenterShowed", false, 0.5);
           break;
       }
 } else {
     setCookie("LatestPost", latestPost, 30);
     setCookie("sameLatest", 1, 30);
+    setCookie("NotificationCenterShowed", false, 0.5);
     console.log("Cookie SET");
 }
